7757bae4ecc3a24ea07f3cbf0cf08335b633f60c
Michael Gatewood <michael@mngatewood.com>
2018-05-30 09:00:05 -0600

    Adds header bars (broken)

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ const toggleCodeFlexibleCards = () => {
 }
 
 $("#flexible-cards__toggle-code-button").click(toggleCodeFlexibleCards)
+
+const toggleCodeHeaderBar = () => {
+  const text = $("#header-bar__toggle-code-button").text()
+  $('#header-bar__code-container').toggleClass('hidden');
+  $("#header-bar__toggle-code-button").text(
+    text === 'HIDE CODE' ? 'VIEW CODE' : 'HIDE CODE'
+  )
+}
+
+$("#header-bar__toggle-code-button").click(toggleCodeHeaderBar)
+
+const toggleHiddenMenu = () => {
+  $('#header-bar--mobile-menu').slideToggle("slow");
+  $('#header-bar--nav').slideToggle("slow");
+  $('#header-bar--search').slideToggle("slow");
+}
